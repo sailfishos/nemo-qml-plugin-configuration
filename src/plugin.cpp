a17a855306fe1f0015bae320c95510fc4df1ed9b
Martin Jones <martin.jones@jollamobile.com>
2013-05-02 03:33:58 +0000

    [configuration] Port to Qt 5.

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -30,14 +30,27 @@
  */
 
 #include <QtGlobal>
-#include <QtDeclarative>
-#include <QDeclarativeEngine>
-#include <QDeclarativeExtensionPlugin>
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+# include <QtQml>
+# include <QQmlEngine>
+# include <QQmlExtensionPlugin>
+# define QDeclarativeEngine QQmlEngine
+# define QDeclarativeExtensionPlugin QQmlExtensionPlugin
+#else
+# include <QtDeclarative>
+# include <QDeclarativeEngine>
+# include <QDeclarativeExtensionPlugin>
+#endif
 
 #include "configurationvalue.h"
 
 class Q_DECL_EXPORT NemoConfigurationValuePlugin : public QDeclarativeExtensionPlugin
 {
+    Q_OBJECT
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+    Q_PLUGIN_METADATA(IID "org.nemomobile.configuration")
+#endif
+
 public:
     virtual ~NemoConfigurationValuePlugin() { }
 
@@ -48,5 +61,8 @@ public:
     }
 };
 
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
 Q_EXPORT_PLUGIN2(nemoconfiguration, NemoConfigurationValuePlugin);
+#endif
 
+#include "plugin.moc"
--- src/src.pro
+++ src/src.pro
@@ -3,13 +3,16 @@ PLUGIN_IMPORT_PATH = org/nemomobile/configuration
 
 TEMPLATE = lib
 CONFIG += qt plugin hide_symbols
-QT += declarative
+equals(QT_MAJOR_VERSION, 4): QT += declarative
+equals(QT_MAJOR_VERSION, 5): QT += qml
+QT -= gui
 
-target.path = $$[QT_INSTALL_IMPORTS]/$$PLUGIN_IMPORT_PATH
+equals(QT_MAJOR_VERSION, 4): target.path = $$[QT_INSTALL_IMPORTS]/$$PLUGIN_IMPORT_PATH
+equals(QT_MAJOR_VERSION, 5): target.path = $$[QT_INSTALL_QML]/$$PLUGIN_IMPORT_PATH
 INSTALLS += target
 
 qmldir.files += $$_PRO_FILE_PWD_/qmldir
-qmldir.path +=  $$[QT_INSTALL_IMPORTS]/$$$$PLUGIN_IMPORT_PATH
+qmldir.path +=  $$target.path
 INSTALLS += qmldir
 
 SOURCES += plugin.cpp \
@@ -18,4 +21,5 @@ SOURCES += plugin.cpp \
 HEADERS += configurationvalue.h
 
 CONFIG += link_pkgconfig
-PKGCONFIG += mlite
+equals(QT_MAJOR_VERSION, 4): PKGCONFIG += mlite
+equals(QT_MAJOR_VERSION, 5): PKGCONFIG += mlite5
